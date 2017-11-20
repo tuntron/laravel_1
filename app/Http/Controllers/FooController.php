59c64cbb2e03d0c7f072f08fd9d3885426147c8e
chenchen <860664331@qq.com>
2017-11-20 16:27:34 +0800

    add log

--- app/Http/Controllers/FooController.php
+++ app/Http/Controllers/FooController.php
@@ -13,6 +13,13 @@ class FooController extends Controller
     {
         $foo = Foo::findOrfail(1);
         for ($i=0;$i<1000;$i++){
+            $flight = new Foo;
+
+            $flight->id = rand(10,1000000);
+
+            $flight->name = 'chenchen'.rand(10,1000000);
+
+            $flight->save();
             $this->dispatch(new SendReminderEmail());
         }
         return $foo;
