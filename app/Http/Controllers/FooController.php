ea176228177849ca4297b1c26c7da3e6a0fdfb41
chenchen <860664331@qq.com>
2017-11-20 16:33:05 +0800

    add log

--- app/Http/Controllers/FooController.php
+++ app/Http/Controllers/FooController.php
@@ -11,11 +11,10 @@ class FooController extends Controller
     //
     public function index()
     {
-        $foo = Foo::findOrfail(1);
         for ($i=0;$i<1000;$i++){
             $this->dispatch(new SendReminderEmail());
         }
-        return $foo;
+        return 1;
     }
 
 	
